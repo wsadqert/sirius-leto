717aa873efc74b017f24afb1beed57791cf05e0e
Matvey Polubryukhov <50608884+wsadqert@users.noreply.github.com>
2024-02-27 22:05:24 +0300

    updated build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,22 @@
 Clear-Host
-pyinstaller --onefile main.py --add-data assets:assets --paths src/
-Remove-Item dist/assets -Recurse -Force -ErrorAction Ignore
-Copy-Item assets dist/
-Remove-Item dist/dist.zip -Recurse -Force -ErrorAction Ignore
-Remove-Item dist/datastore -Recurse -Force -ErrorAction Ignore
-7z a -tzip dist/dist.zip dist/
+
+$current_directory = Get-Location
+
+# compiling
+pyinstaller --onefile "main.py" --add-data assets:assets --paths src/ --paths venv/Lib/site-packages
+pyinstaller --onefile "main_gui.py" --add-data assets:assets --paths src/ --paths venv/Lib/site-packages
+
+# cleaning previous version of `assets`
+cd dist/
+
+Remove-Item assets -Recurse -Force -ErrorAction Ignore
+Copy-Item ../assets . -Recurse
+Remove-Item dist.zip -Recurse -Force -ErrorAction Ignor
+Remove-Item datastore -Recurse -Force -ErrorAction Ignore
+
+Write "assets`r`n main.exe`r`n main_gui.exe" | Out-File listfile.txt
+
+# compressing
+cmd /c "7z a -mx1 -mmt=12 -tzip dist.zip @listfile.txt"
+
+cd $current_directory
