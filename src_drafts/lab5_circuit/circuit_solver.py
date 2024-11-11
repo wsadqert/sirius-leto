0af8a262d37526378080aaa2d90f168bf95ff6f2
Matvey Polubryukhov <50608884+wsadqert@users.noreply.github.com>
2024-11-12 01:47:17 +0300

    implemented diode

--- src_drafts/lab5_circuit/circuit_solver.py
+++ src_drafts/lab5_circuit/circuit_solver.py
@@ -1,89 +1,116 @@
+from pprint import pprint
 import numpy as np
 
 class Resistor:
-    def __init__(self, resistance, node1, node2):
-        self.resistance = resistance
-        self.node1 = node1
-        self.node2 = node2
+	def __init__(self, resistance, node1, node2):
+		self.resistance = resistance
+		self.node1 = node1
+		self.node2 = node2
 
 class VoltageSource:
-    def __init__(self, voltage, node1, node2):
-        self.voltage = voltage
-        self.node1 = node1
-        self.node2 = node2
+	def __init__(self, voltage, node1, node2):
+		self.voltage = voltage
+		self.node1 = node1
+		self.node2 = node2
+
+class Diode:
+	def __init__(self, node1, node2):
+		self.node1 = node1  # Anode
+		self.node2 = node2  # Cathode
+
 
 class Circuit:
-    def __init__(self):
-        self.resistors = []
-        self.voltage_sources = []
-        self.nodes = set()
+	def __init__(self):
+		self.resistors = []
+		self.voltage_sources = []
+		self.diodes = []
+		self.nodes = set()
 
-    def add_resistor(self, resistance, node1, node2):
-        self.resistors.append(Resistor(resistance, node1, node2))
-        self.nodes.update([node1, node2])
+	def add_resistor(self, resistance, node1, node2):
+		self.resistors.append(Resistor(resistance, node1, node2))
+		self.nodes.update([node1, node2])
 
-    def add_voltage_source(self, voltage, node1, node2):
-        self.voltage_sources.append(VoltageSource(voltage, node1, node2))
-        self.nodes.update([node1, node2])
+	def add_voltage_source(self, voltage, node1, node2):
+		self.voltage_sources.append(VoltageSource(voltage, node1, node2))
+		self.nodes.update([node1, node2])
 
-    def solve(self):
-        node_list = list(self.nodes)
-        node_index = {node: i for i, node in enumerate(node_list)}
-        num_nodes = len(node_list)
+	def add_diode(self, node1, node2):
+		self.diodes.append(Diode(node1, node2))
+		self.nodes.update([node1, node2])
 
-        # Create the matrix A and vector B
-        A = np.zeros((num_nodes, num_nodes))
-        B = np.zeros(num_nodes)
+	def solve(self):
+		node_list = list(self.nodes)
+		node_index = {node: i for i, node in enumerate(node_list)}
+		num_nodes = len(node_list)
 
-        # Treat 'ground' as the reference node
-        if 'ground' not in node_index:
-            raise ValueError("Ground node must be defined as 'ground'.")
+		# Create the matrix A and vector B
+		A = np.zeros((num_nodes, num_nodes))
+		B = np.zeros(num_nodes)
 
-        # Build equations based on KCL for each node (except ground)
-        for node in node_list:
-            if node == 'ground':
-                continue
+		# Treat 'ground' as the reference node
+		if 'ground' not in node_index:
+			raise ValueError("Ground node must be defined as 'ground'.")
 
-            node_eq_index = node_index[node]
-            for resistor in self.resistors:
-                if resistor.node1 == node:
-                    other_node_index = node_index[resistor.node2]
-                    A[node_eq_index, node_eq_index] += 1 / resistor.resistance
-                    A[node_eq_index, other_node_index] -= 1 / resistor.resistance
-                elif resistor.node2 == node:
-                    other_node_index = node_index[resistor.node1]
-                    A[node_eq_index, node_eq_index] += 1 / resistor.resistance
-                    A[node_eq_index, other_node_index] -= 1 / resistor.resistance
+		# Build equations based on KCL for each node (except ground)
+		for node in node_list:
+			if node == 'ground':
+				continue
 
-            for source in self.voltage_sources:
-                if source.node1 == node:
-                    other_node_index = node_index[source.node2]
-                    A[node_eq_index, node_eq_index] += 1  # KVL for voltage source
-                    A[node_eq_index, other_node_index] -= 1  # KVL for voltage source
-                    B[node_eq_index] -= source.voltage
-                elif source.node2 == node:
-                    other_node_index = node_index[source.node1]
-                    A[node_eq_index, node_eq_index] += 1  # KVL for voltage source
-                    A[node_eq_index, other_node_index] -= 1  # KVL for voltage source
-                    B[node_eq_index] += source.voltage
+			node_eq_index = node_index[node]
+			for resistor in self.resistors:
+				if resistor.node1 == node:
+					other_node_index = node_index[resistor.node2]
+					A[node_eq_index, node_eq_index] += 1 / resistor.resistance
+					A[node_eq_index, other_node_index] -= 1 / resistor.resistance
+				elif resistor.node2 == node:
+					other_node_index = node_index[resistor.node1]
+					A[node_eq_index, node_eq_index] += 1 / resistor.resistance
+					A[node_eq_index, other_node_index] -= 1 / resistor.resistance
 
-        # Set ground node voltage to 0
-        ground_index = node_index['ground']
-        A[ground_index, :] = 0
-        A[ground_index, ground_index] = 1
-        B[ground_index] = 0
+			for source in self.voltage_sources:
+				if source.node1 == node:
+					other_node_index = node_index[source.node2]
+					A[node_eq_index, node_eq_index] += 1  # KVL for voltage source
+					A[node_eq_index, other_node_index] -= 1  # KVL for voltage source
+					B[node_eq_index] -= source.voltage
+				elif source.node2 == node:
+					other_node_index = node_index[source.node1]
+					A[node_eq_index, node_eq_index] += 1  # KVL for voltage source
+					A[node_eq_index, other_node_index] -= 1  # KVL for voltage source
+					B[node_eq_index] += source.voltage
 
-        # Enforce the voltage of the voltage sources
-        for source in self.voltage_sources:
-            if source.node1 == 'ground':
-                node_eq_index = node_index[source.node2]
-                A[node_eq_index, :] = 0  # Clear the equation for this node
-                A[node_eq_index, node_eq_index] = 1  # Set the voltage at this node
-                B[node_eq_index] = source.voltage  # Set the voltage to the source voltage
+			for diode in self.diodes:
+				if diode.node1 == node:  # Diode is forward biased
+					other_node_index = node_index[diode.node2]
+					A[node_eq_index, node_eq_index] += 1  # Current can flow
+					A[node_eq_index, other_node_index] -= 1  # Current can flow
+				elif diode.node2 == node:  # Diode is reverse biased
+					other_node_index = node_index[diode.node1]
+					# No changes to A or B since current cannot flow
 
-        # Solve the linear equations
-        voltages = np.linalg.solve(A, B)
-        return {node: voltages[i] for i, node in enumerate(node_list)}
+
+		# Set ground node voltage to 0
+		ground_index = node_index['ground']
+		A[ground_index, :] = 0
+		A[ground_index, ground_index] = 1
+		B[ground_index] = 0
+
+		# Enforce the voltage of the voltage sources
+		for source in self.voltage_sources:
+			if source.node1 == 'ground':
+				node_eq_index = node_index[source.node2]
+				A[node_eq_index, :] = 0  # Clear the equation for this node
+				A[node_eq_index, node_eq_index] = 1  # Set the voltage at this node
+				B[node_eq_index] = source.voltage  # Set the voltage to the source voltage
+			elif source.node2 == 'ground':
+				node_eq_index = node_index[source.node1]
+				A[node_eq_index, :] = 0  # Clear the equation for this node
+				A[node_eq_index, node_eq_index] = 1  # Set the voltage at this node
+				B[node_eq_index] = -source.voltage  # Set the voltage to the source voltage (negative)
+
+		# Solve the linear equations
+		voltages = np.linalg.solve(A, B)
+		return {node: voltages[i] for i, node in enumerate(node_list)}
 
 # Example usage
 circuit = Circuit()
@@ -91,6 +118,7 @@ circuit.add_voltage_source(10, 'ground', 'node1')
 circuit.add_resistor(10, 'node1', 'node2')
 circuit.add_resistor(3, 'node2', 'ground')
 circuit.add_resistor(5, 'node2', 'node3')
-circuit.add_resistor(2, 'node3', 'ground')
+circuit.add_diode('node3', 'ground')
+circuit.add_diode('ground', 'node3')
 
-print(circuit.solve())
+pprint(circuit.solve())
